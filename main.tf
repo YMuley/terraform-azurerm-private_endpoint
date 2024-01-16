f70403e179b6f2f2b0bf708c0a1d36408543f25a
sunanda sharma <sunandasharma@ddiworld.com>
2024-01-16 16:38:13 +0530

    updated

--- main.tf
+++ main.tf
@@ -8,21 +8,11 @@ resource "azurerm_private_endpoint" "private_endpoint" {
   subnet_id           = var.subnet_output[format("%s/%s", each.value.virtual_network_name, each.value.subnet_name)].id
   tags                = each.value.tags == null ? var.default_values.tags : each.value.tags
 
-  #------ Storage Account 
-  # dynamic "private_service_connection" {
-  #   for_each = each.value.storage_account
-  #   content {
-  #     name                           = "private_service_connection_${each.value.name}"
-  #     is_manual_connection           = false
-  #     private_connection_resource_id = var.storage_account_output[each.value.storage_account_name].id
-  #   }
-  # }
-
   private_service_connection {
 
     name                           = "private_service_connection_${each.value.name}"
     is_manual_connection           = false
-    private_connection_resource_id = var.storage_account_output[each.value.storage_account_name].id
+    private_connection_resource_id = each.value.private_connection_resource_id
     subresource_names              = each.value.subresource_name
   }
 }
